b8566f5dc6c28c52605222fd314cb15d9694edea
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-03-21 14:54:05 +0100

    Revert "Use common SHA2 from pqclean"
    This reverts commit 23d7542728e72505a68056db90c9ccc8dbd9afd5.
    We need a modified SHA2 for hash profiling.

--- common/randombytes.h
+++ common/randombytes.h
@@ -1,1 +1,6 @@
-../pqclean/common/randombytes.h
+#ifndef RANDOMBYTES_H
+#define RANDOMBYTES_H
+
+void randombytes(unsigned char *x,unsigned long long xlen);
+
+#endif
--- common/sha2.h
+++ common/sha2.h
@@ -1,1 +1,9 @@
-../pqclean/common/sha2.h
+#ifndef SHA2_H
+#define SHA2_H
+
+int sha384(unsigned char *output, const unsigned char *input, unsigned long long inlen);
+int sha512(unsigned char *output, const unsigned char *input, unsigned long long inlen);
+
+#endif
+
+
