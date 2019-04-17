5d9601fca3a97d79ef544ea2d92dfff510da37e5
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-04-17 13:58:52 +0200

    fix incremental keccak implementation

--- common/fips202.c
+++ common/fips202.c
@@ -119,15 +119,10 @@ static void keccak_inc_init(uint64_t *s_inc) {
  **************************************************/
 static void keccak_inc_absorb(uint64_t *s_inc, uint32_t r, const uint8_t *m,
                               size_t mlen) {
-    size_t i;
-
     /* Recall that s_inc[25] is the non-absorbed bytes xored into the state */
     while (mlen + s_inc[25] >= r) {
-        for (i = 0; i < r - s_inc[25]; i++) {
-            /* Take the i'th byte from message
-               xor with the s_inc[25] + i'th byte of the state; little-endian */
-            s_inc[(s_inc[25] + i) >> 3] ^= (uint64_t)m[i] << (8 * ((s_inc[25] + i) & 0x07));
-        }
+
+        KeccakF1600_StateXORBytes(s_inc, m, s_inc[25], r-s_inc[25]);
         mlen -= (size_t)(r - s_inc[25]);
         m += r - s_inc[25];
         s_inc[25] = 0;
@@ -135,9 +130,7 @@ static void keccak_inc_absorb(uint64_t *s_inc, uint32_t r, const uint8_t *m,
         KeccakF1600_StatePermute(s_inc);
     }
 
-    for (i = 0; i < mlen; i++) {
-        s_inc[(s_inc[25] + i) >> 3] ^= (uint64_t)m[i] << (8 * ((s_inc[25] + i) & 0x07));
-    }
+    KeccakF1600_StateXORBytes(s_inc, m, s_inc[25], mlen);
     s_inc[25] += mlen;
 }
 
@@ -157,9 +150,14 @@ static void keccak_inc_absorb(uint64_t *s_inc, uint32_t r, const uint8_t *m,
 static void keccak_inc_finalize(uint64_t *s_inc, uint32_t r, uint8_t p) {
     /* After keccak_inc_absorb, we are guaranteed that s_inc[25] < r,
        so we can always use one more byte for p in the current state. */
-    s_inc[s_inc[25] >> 3] ^= (uint64_t)p << (8 * (s_inc[25] & 0x07));
-    s_inc[(r - 1) >> 3] ^= (uint64_t)128 << (8 * ((r - 1) & 0x07));
-    s_inc[25] = 0;
+    size_t i;
+    unsigned char t[200];
+    for (i = 0; i < r; ++i)
+      t[i] = 0;
+    t[s_inc[25]] = p;
+    t[r - 1] |= 128;
+
+    KeccakF1600_StateXORBytes(s_inc, t, 0, r);
 }
 
 /*************************************************
@@ -177,28 +175,37 @@ static void keccak_inc_finalize(uint64_t *s_inc, uint32_t r, uint8_t p) {
  **************************************************/
 static void keccak_inc_squeeze(uint8_t *h, size_t outlen,
                                uint64_t *s_inc, uint32_t r) {
-    size_t i;
-
-    /* First consume any bytes we still have sitting around */
-    for (i = 0; i < outlen && i < s_inc[25]; i++) {
-        /* There are s_inc[25] bytes left, so r - s_inc[25] is the first
-           available byte. We consume from there, i.e., up to r. */
-        h[i] = (uint8_t)(s_inc[(r - s_inc[25] + i) >> 3] >> (8 * ((r - s_inc[25] + i) & 0x07)));
+    size_t len;
+    if(outlen < s_inc[25])
+    {
+        len = outlen;
     }
-    h += i;
-    outlen -= i;
-    s_inc[25] -= i;
+    else
+    {
+        len = s_inc[25];
+    }
+
+    KeccakF1600_StateExtractBytes(s_inc, h, r-s_inc[25], len);
+    h += len;
+    outlen -= len;
+    s_inc[25] -= len;
 
     /* Then squeeze the remaining necessary blocks */
     while (outlen > 0) {
         KeccakF1600_StatePermute(s_inc);
 
-        for (i = 0; i < outlen && i < r; i++) {
-            h[i] = (uint8_t)(s_inc[i >> 3] >> (8 * (i & 0x07)));
+        if(outlen < r)
+        {
+            len = outlen;
         }
-        h += i;
-        outlen -= i;
-        s_inc[25] = r - i;
+        else
+        {
+            len = r;
+        }
+        KeccakF1600_StateExtractBytes(s_inc, h, 0, len);
+        h += len;
+        outlen -= len;
+        s_inc[25] = r - len;
     }
 }
 
