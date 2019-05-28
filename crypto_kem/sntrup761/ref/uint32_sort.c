25a421710dd74ffad25c51ee659eaa7e87d0396a
Ko- <k.stoffelen@cs.ru.nl>
2019-05-28 17:16:58 +0200

    Add sntrup761 Round 2

---
+++ crypto_kem/sntrup761/ref/uint32_sort.c
@@ -0,0 +1,34 @@
+#include "uint32.h"
+
+static void minmax(uint32 *x,uint32 *y)
+{
+  uint32 xi = *x;
+  uint32 yi = *y;
+  uint32 xy = xi ^ yi;
+  uint32 c = yi - xi;
+  c ^= xy & (c ^ yi ^ 0x80000000);
+  c >>= 31;
+  c = -c;
+  c &= xy;
+  *x = xi ^ c;
+  *y = yi ^ c;
+}
+
+void uint32_sort(uint32 *x,int n)
+{
+  int top,p,q,i;
+
+  if (n < 2) return;
+  top = 1;
+  while (top < n - top) top += top;
+
+  for (p = top;p > 0;p >>= 1) {
+    for (i = 0;i < n - p;++i)
+      if (!(i & p))
+        minmax(x + i,x + i + p);
+    for (q = top;q > p;q >>= 1)
+      for (i = 0;i < n - q;++i)
+        if (!(i & p))
+          minmax(x + i + p,x + i + q);
+  }
+}
