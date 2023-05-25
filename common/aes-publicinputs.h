2425e129957321f6ae629ac28dbe2c34bdf268ce
Matthias J. Kannwischer <matthias@kannwischer.eu>
2023-05-25 15:36:47 +0800

    add aes-publicinputs

---
+++ common/aes-publicinputs.h
@@ -0,0 +1,49 @@
+#ifndef AES_PUBLICINPUTS_H
+#define AES_PUBLICINPUTS_H
+
+
+#include "aes.h"
+
+
+#define aes128ctx_publicinputs aes128ctx
+#define aes192ctx_publicinputs aes192ctx
+#define aes256ctx_publicinputs aes256ctx
+
+
+
+/** Initializes the context **/
+#define aes128_ecb_keyexp_publicinputs aes128_ecb_keyexp
+
+#define aes128_ctr_keyexp_publicinputs aes128_ctr_keyexp
+
+#define aes128_ecb_publicinputs aes128_ecb
+
+#define aes128_ctr_publicinputs aes128_ctr
+
+#define aes128_ctx_release_publicinputs aes128_ctx_release
+
+/** Initializes the context **/
+#define aes192_ecb_keyexp_publicinputs aes192_ecb_keyexp
+
+#define aes192_ctr_keyexp_publicinputs aes192_ctr_keyexp
+
+#define aes192_ecb_publicinputs aes192_ecb
+
+#define aes192_ctr_publicinputs aes192_ctr
+
+#define aes192_ctx_release_publicinputs aes192_ctx_release
+
+
+/** Initializes the context **/
+#define aes256_ecb_keyexp_publicinputs aes256_ecb_keyexp
+
+#define aes256_ctr_keyexp_publicinputs aes256_ctr_keyexp
+
+#define aes256_ecb_publicinputs aes256_ecb
+
+#define aes256_ctr_publicinputs aes256_ctr
+
+#define aes256_ctx_release_publicinputs aes256_ctx_release
+
+
+#endif
