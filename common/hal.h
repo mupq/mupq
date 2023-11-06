4f7b229af9bb4d096f8cdd7cae67a59e0f197160
Richard Petri <git@rpls.de>
2023-11-06 21:43:51 +0100

    New HAL function for stack spraying/checking

--- common/hal.h
+++ common/hal.h
@@ -13,5 +13,7 @@ void hal_setup(const enum clock_mode clock);
 void hal_send_str(const char* in);
 uint64_t hal_get_time(void);
 size_t hal_get_stack_size(void);
+void hal_spraystack(void);
+size_t hal_checkstack(void);
 
 #endif
