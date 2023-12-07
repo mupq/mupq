c38668b687ee85d24c0b10a19c9b8f67cf29fdda
Matthias J. Kannwischer <matthias@kannwischer.eu>
2023-12-07 13:10:36 +0800

    first working version

---
+++ crypto_sign/sdith_hypercube_cat1_gf256/ref/hash-sha3.c
@@ -0,0 +1,100 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "param.h"
+#include "rng.h"
+#include "fips202.h"
+
+
+
+HASH_CTX *sdith_hash_create_hash_ctx(uint8_t prefix) {
+  // TODO other sec levels
+  sha3_256incctx *inst =
+      (sha3_256incctx *)malloc(sizeof(sha3_256incctx));
+#if defined(CAT_1)
+  //Keccak_HashInitialize_SHA3_256(inst);
+  sha3_256_inc_init(inst);
+#elif defined(CAT_3)
+  Keccak_HashInitialize_SHA3_384(inst);
+#else
+  Keccak_HashInitialize_SHA3_512(inst);
+#endif
+  sha3_256_inc_absorb(inst, &prefix, 1);
+  //Keccak_HashUpdate(inst, &prefix, sizeof(uint8_t) << 3);
+  return (HASH_CTX *)inst;
+}
+
+void sdith_hash_free_hash_ctx(HASH_CTX *ctx) { free(ctx); }
+
+void sdith_hash_digest_update(HASH_CTX *ctx, void const *in, int inBytes) {
+  //Keccak_HashUpdate((Keccak_HashInstance *)ctx, (uint8_t const *)in,
+  //                  inBytes << 3);
+  sha3_256_inc_absorb((sha3_256incctx *)ctx, in, inBytes);
+}
+
+void sdith_hash_final(HASH_CTX *ctx, void *dest) {
+  //Keccak_HashFinal((Keccak_HashInstance *)ctx, dest);
+  sha3_256_inc_finalize(dest, ctx);
+}
+
+void sdith_hash(uint8_t prefix, void *dest, void const *data, int dataBytes) {
+  HASH_CTX *ctx = sdith_hash_create_hash_ctx(prefix);
+  sdith_hash_digest_update(ctx, data, dataBytes);
+  sdith_hash_final(ctx, dest);
+  sdith_hash_free_hash_ctx(ctx);
+}
+
+
+HASH4_CTX *sdith_hash_create_hash4_ctx(uint8_t prefix) {
+  sha3_256incctx *hash4_ctx = (sha3_256incctx *)malloc(4*sizeof(sha3_256incctx));
+
+#if defined(CAT_1)
+  sha3_256_inc_init(&hash4_ctx[0]);
+  sha3_256_inc_init(&hash4_ctx[1]);
+  sha3_256_inc_init(&hash4_ctx[2]);
+  sha3_256_inc_init(&hash4_ctx[3]);
+#elif defined(CAT_3)
+  Keccak_HashInitializetimes4_SHA3_384(&hash4_ctx);
+#else
+  Keccak_HashInitializetimes4_SHA3_512(&hash4_ctx);
+#endif
+  sha3_256_inc_absorb(&hash4_ctx[0], &prefix, 1);
+  sha3_256_inc_absorb(&hash4_ctx[1], &prefix, 1);
+  sha3_256_inc_absorb(&hash4_ctx[2], &prefix, 1);
+  sha3_256_inc_absorb(&hash4_ctx[3], &prefix, 1);
+  //const uint8_t *prefix_ptr[4] = {&prefix, &prefix, &prefix, &prefix};
+  //Keccak_HashUpdatetimes4(&hash4_ctx, prefix_ptr, sizeof(prefix) << 3);
+  return (HASH4_CTX *)hash4_ctx;
+}
+
+void sdith_hash_free_hash4_ctx(HASH4_CTX *ctx) { free(ctx); }
+
+void sdith_hash4_digest_update(HASH4_CTX *ctx, void **in, int inBytes) {
+  sha3_256incctx *hash4_ctx = ctx;
+  sha3_256_inc_absorb(&hash4_ctx[0], in[0], inBytes);
+  sha3_256_inc_absorb(&hash4_ctx[1], in[1], inBytes);
+  sha3_256_inc_absorb(&hash4_ctx[2], in[2], inBytes);
+  sha3_256_inc_absorb(&hash4_ctx[3], in[3], inBytes);
+
+
+  //Keccak_HashUpdatetimes4((Keccak_HashInstancetimes4 *)ctx,
+  //                        (const uint8_t **)in, inBytes << 3);
+}
+
+void sdith_hash4_final(HASH4_CTX *ctx, void **dest) {
+  //Keccak_HashFinaltimes4((Keccak_HashInstancetimes4 *)ctx, (uint8_t **)dest);
+  sha3_256incctx *hash4_ctx = ctx;
+
+  sha3_256_inc_finalize(dest[0], &hash4_ctx[0]);
+  sha3_256_inc_finalize(dest[1], &hash4_ctx[1]);
+  sha3_256_inc_finalize(dest[2], &hash4_ctx[2]);
+  sha3_256_inc_finalize(dest[3], &hash4_ctx[3]);
+}
+
+void sdith_hash4(uint8_t prefix, void **dest, void **data, int dataBytes) {
+  HASH4_CTX *ctx = sdith_hash_create_hash4_ctx(prefix);
+  sdith_hash4_digest_update(ctx, data, dataBytes);
+  sdith_hash4_final(ctx, dest);
+  sdith_hash_free_hash4_ctx(ctx);
+}
---
+++ crypto_sign/sdith_hypercube_cat1_gf256/ref/rng.c
@@ -0,0 +1,129 @@
+#include "rng.h"
+
+#include "param.h"
+#include "types.h"
+#include "fips202.h"
+#include <stdlib.h>
+#include <string.h>
+
+EXPORT XOF_CTX *sdith_rng_create_xof_ctx(void *in, int inBytes) {
+  // TODO: other security levels
+  shake128incctx *inst =
+      (shake128incctx *)malloc(sizeof(shake128incctx));
+#if defined(CAT_1)
+  //Keccak_HashInitialize_SHAKE128(inst);
+  shake128_inc_init(inst);
+#else
+  Keccak_HashInitialize_SHAKE256(inst);
+#endif
+  //Keccak_HashUpdate(inst, in, inBytes << 3);
+  shake128_inc_absorb(inst, in, inBytes);
+  //Keccak_HashFinal(inst, NULL);
+  shake128_inc_finalize(inst);
+  return (XOF_CTX *)inst;
+}
+
+EXPORT void sdith_rng_free_xof_ctx(XOF_CTX *ctx) { free(ctx); }
+
+EXPORT void sdith_xof_next_bytes(XOF_CTX *ctx, void *out, int outLen) {
+  //Keccak_HashSqueeze((Keccak_HashInstance *)ctx, out, outLen << 3);
+  shake128_inc_squeeze(out, outLen, ctx);
+}
+
+EXPORT void sdith_xof_next_bytes_mod251(XOF_CTX *ctx, void *out, int outLen) {
+  // Roughly sample 1.03x of original length, which is greater than 256/251.
+  int len = outLen + (outLen >> 5);
+  uint8_t *buf = (uint8_t *)malloc(len);
+  sdith_xof_next_bytes(ctx, buf, len);
+  int bytes_remaining = len;
+  uint8_t *buf_ptr = buf;
+  uint8_t *out_buf = (uint8_t *)out;
+  int counter = 0;
+  while (counter < outLen) {
+    if (*buf_ptr < 251) {
+      out_buf[counter++] = *buf_ptr;
+    }
+    buf_ptr++;
+    bytes_remaining--;
+    if (bytes_remaining == 0) {
+      sdith_xof_next_bytes(ctx, buf, len);
+      bytes_remaining = len;
+      buf_ptr = buf;
+    }
+  }
+  free(buf);
+}
+
+// TODO: move this outside
+shake128incctx states[4];
+
+EXPORT XOF4_CTX *sdith_rng_create_xof4_ctx(void **in, int inBytes) {
+  //shake128incctx *xof4_ctx =
+  //    (shake128incctx *)malloc(4*sizeof(shake128incctx));
+  shake128incctx *xof4_ctx = states;
+
+#if defined(CAT_1)
+  //Keccak_HashInitializetimes4_SHAKE128(&xof4_ctx);
+  shake128_inc_init(&xof4_ctx[0]);
+  shake128_inc_init(&xof4_ctx[1]);
+  shake128_inc_init(&xof4_ctx[2]);
+  shake128_inc_init(&xof4_ctx[3]);
+#else
+  Keccak_HashInitializetimes4_SHAKE256(&xof4_ctx);
+#endif
+  //Keccak_HashUpdatetimes4(&xof4_ctx, (const uint8_t **)in, inBytes << 3);
+  shake128_inc_absorb(&xof4_ctx[0], in[0], inBytes);
+  shake128_inc_absorb(&xof4_ctx[1], in[1], inBytes);
+  shake128_inc_absorb(&xof4_ctx[2], in[2], inBytes);
+  shake128_inc_absorb(&xof4_ctx[3], in[3], inBytes);
+
+  //Keccak_HashFinaltimes4(&xof4_ctx, NULL);
+  shake128_inc_finalize(&xof4_ctx[0]);
+  shake128_inc_finalize(&xof4_ctx[1]);
+  shake128_inc_finalize(&xof4_ctx[2]);
+  shake128_inc_finalize(&xof4_ctx[3]);
+
+  return (XOF4_CTX *)&xof4_ctx;
+}
+
+EXPORT void sdith_rng_free_xof4_ctx(XOF4_CTX *ctx) { (void) ctx; }
+
+EXPORT void sdith_xof4_next_bytes(XOF4_CTX *ctx, void **out, int outLen) {
+  // Keccak_HashSqueezetimes4((Keccak_HashInstancetimes4 *)ctx, (uint8_t **)out,
+  //                          outLen << 3);
+  shake128incctx *xof4_ctx = states;
+  shake128_inc_squeeze(out[0], outLen, &xof4_ctx[0]);
+  shake128_inc_squeeze(out[1], outLen, &xof4_ctx[1]);
+  shake128_inc_squeeze(out[2], outLen, &xof4_ctx[2]);
+  shake128_inc_squeeze(out[3], outLen, &xof4_ctx[3]);
+}
+
+EXPORT void sdith_xof4_next_bytes_mod251(XOF4_CTX *ctx, void **out,
+                                         int outLen) {
+  // Roughly sample 1.03x of original length, which is greater than 256/251.
+  int len = outLen + (outLen >> 5);
+  uint8_t *buf = (uint8_t *)malloc(len * 4);
+  uint8_t *bufs[4] = {&buf[0], &buf[len], &buf[len * 2], &buf[len * 3]};
+  int bytes_counter[4] = {0};
+  while (bytes_counter[0] + bytes_counter[1] + bytes_counter[2] +
+             bytes_counter[3] <
+         outLen * 4) {
+    sdith_xof4_next_bytes(ctx, (void **)bufs, len);
+    for (uint64_t i = 0; i < 4; ++i) {
+      uint8_t *buf_ptr = bufs[i];
+      uint8_t *out_buf = (uint8_t *)out[i];
+      int bytes_remaining = len;
+      while (bytes_counter[i] < outLen) {
+        if (*buf_ptr < 251) {
+          out_buf[bytes_counter[i]++] = *buf_ptr;
+        }
+        buf_ptr++;
+        bytes_remaining--;
+        if (bytes_remaining == 0) {
+          break;
+        }
+      }
+    }
+  }
+  free(buf);
+}
