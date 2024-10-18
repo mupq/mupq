c3395f274bf950ca716bcb24ab0a8b9f4cc85740
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-10-18 16:42:54 +0800

    Adjust speed.c for M7

--- crypto_kem/speed.c
+++ crypto_kem/speed.c
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: Apache-2.0 or CC0-1.0
+// SPDX-License-Identifier: Apache-2.0
 #include "api.h"
 #include "hal.h"
+#include "randombytes.h"
 #include "sendfn.h"
 
 #include <stdint.h>
@@ -12,9 +13,9 @@
 #define NAMESPACE(fun) EVALUATOR(MUPQ_NAMESPACE, fun)
 
 // use different names so we can have empty namespaces
-#define MUPQ_CRYPTO_BYTES           NAMESPACE(CRYPTO_BYTES)
-#define MUPQ_CRYPTO_PUBLICKEYBYTES  NAMESPACE(CRYPTO_PUBLICKEYBYTES)
-#define MUPQ_CRYPTO_SECRETKEYBYTES  NAMESPACE(CRYPTO_SECRETKEYBYTES)
+#define MUPQ_CRYPTO_BYTES NAMESPACE(CRYPTO_BYTES)
+#define MUPQ_CRYPTO_PUBLICKEYBYTES NAMESPACE(CRYPTO_PUBLICKEYBYTES)
+#define MUPQ_CRYPTO_SECRETKEYBYTES NAMESPACE(CRYPTO_SECRETKEYBYTES)
 #define MUPQ_CRYPTO_CIPHERTEXTBYTES NAMESPACE(CRYPTO_CIPHERTEXTBYTES)
 #define MUPQ_CRYPTO_ALGNAME NAMESPACE(CRYPTO_ALGNAME)
 
@@ -24,12 +25,17 @@
 
 #define printcycles(S, U) send_unsignedll((S), (U))
 
-int main(void)
-{
+static int cmp_uint64_t(const void *a, const void *b) {
+  return (int)((*((const uint64_t *)a)) - (*((const uint64_t *)b)));
+}
+
+int main(void) {
   unsigned char key_a[MUPQ_CRYPTO_BYTES], key_b[MUPQ_CRYPTO_BYTES];
   unsigned char sk[MUPQ_CRYPTO_SECRETKEYBYTES];
   unsigned char pk[MUPQ_CRYPTO_PUBLICKEYBYTES];
   unsigned char ct[MUPQ_CRYPTO_CIPHERTEXTBYTES];
+  uint64_t cycles_kg[MUPQ_ITERATIONS], cycles_enc[MUPQ_ITERATIONS],
+      cycles_dec[MUPQ_ITERATIONS];
   unsigned long long t0, t1;
   int i;
 
@@ -37,34 +43,43 @@ int main(void)
 
   hal_send_str("==========================");
 
-  for(i=0;i<MUPQ_ITERATIONS; i++)
-  {
+  for (i = 0; i < MUPQ_ITERATIONS; i++) {
     // Key-pair generation
     t0 = hal_get_time();
     MUPQ_crypto_kem_keypair(pk, sk);
     t1 = hal_get_time();
-    printcycles("keypair cycles:", t1-t0);
+    cycles_kg[i] = t1 - t0;
+  }
 
+  for (i = 0; i < MUPQ_ITERATIONS; i++) {
     // Encapsulation
     t0 = hal_get_time();
     MUPQ_crypto_kem_enc(ct, key_a, pk);
     t1 = hal_get_time();
-    printcycles("encaps cycles:", t1-t0);
+    cycles_enc[i] = t1 - t0;
+  }
 
+  for (i = 0; i < MUPQ_ITERATIONS; i++) {
     // Decapsulation
     t0 = hal_get_time();
     MUPQ_crypto_kem_dec(key_b, ct, sk);
     t1 = hal_get_time();
-    printcycles("decaps cycles:", t1-t0);
-
-    if (memcmp(key_a, key_b, MUPQ_CRYPTO_BYTES)) {
-      hal_send_str("ERROR KEYS\n");
-    }
-    else {
-      hal_send_str("OK KEYS\n");
-    }
-    hal_send_str("+");
+    cycles_dec[i] = t1 - t0;
   }
+
+  if (memcmp(key_a, key_b, MUPQ_CRYPTO_BYTES)) {
+    hal_send_str("ERROR KEYS\n");
+  } else {
+    hal_send_str("OK KEYS\n");
+  }
+
+  qsort(cycles_kg, MUPQ_ITERATIONS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_enc, MUPQ_ITERATIONS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_dec, MUPQ_ITERATIONS, sizeof(uint64_t), cmp_uint64_t);
+
+  printcycles("keypair cycles", cycles_kg[MUPQ_ITERATIONS >> 1]);
+  printcycles("encaps cycles", cycles_enc[MUPQ_ITERATIONS >> 1]);
+  printcycles("decaps cycles", cycles_dec[MUPQ_ITERATIONS >> 1]);
 
   hal_send_str("#");
 
